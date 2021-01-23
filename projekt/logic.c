ef6eef26283a6dc6e2034054ea1057244614af5e
FilipKarpowicz <73248709+FilipKarpowicz@users.noreply.github.com>
2021-01-23 11:02:37 +0100

    logika zaczatki

--- projekt/logic.c
+++ projekt/logic.c
@@ -1,6 +1,33 @@
-#include"logic.h"
+#include "logic.h"
 
-void odkryj_mape(mapa *m)
+char *wykonaj_ruch(mapa *stan_mapy, int *bilans_rotacji, int *poczatek_sciany)
 {
-    Dane* dane=(Dane*) malloc(sizeof(Dane));
+    //w prawo +1 w lewo -1
+    if (bilans_rotacji == 4 &&stan_mapy->pozycja_y == poczatek_sciany[0] && stan_mapy->pozycja_x == poczatek_sciany[1])
+    {
+        bilans_rotacji = 0;
+        szukaj_wew_przeszkod(&stan_mapy);
+        return "rotate_left";
+    }
+    if (stan_mapy->kierunek == 'N')
+    {
+        if (stan_mapy->mapa[stan_mapy->pozycja_y - 1][stan_mapy->pozycja_x] == ' ' || stan_mapy->mapa[stan_mapy->pozycja_y - 1][stan_mapy->pozycja_x + 1] == ' ' || stan_mapy->mapa[stan_mapy->pozycja_y - 1][stan_mapy->pozycja_x - 1] == ' ')
+        {
+            return "explore";
+        }
+        else if (stan_mapy->mapa[stan_mapy->pozycja_y - 1][stan_mapy->pozycja_x + 1] == 'W' && stan_mapy->mapa[stan_mapy->pozycja_y - 1][stan_mapy->pozycja_x] != 'W')
+        {
+            return "move";
+        }
+        else if (stan_mapy->mapa[stan_mapy->pozycja_y][stan_mapy->pozycja_x + 1] != 'W' && stan_mapy->mapa[stan_mapy->pozycja_y + 1][stan_mapy->pozycja_x + 1] == 'W')
+        {
+            bilans_rotacji++;
+            return "rotate_right";
+        }
+        else if(stan_mapy->mapa[stan_mapy->pozycja_y-1][stan_mapy->pozycja_x + 1] == 'W' && stan_mapy->mapa[stan_mapy->pozycja_y - 1][stan_mapy->pozycja_x ] == 'W')
+        {
+            return "rotate_left";
+        }
+           
+    }
 }
