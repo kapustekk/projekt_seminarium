3da9090f537dcb28a17d92bf64f03866ac36f701
kapustekk <tomasz.kapuscinski.52@gmail.com>
2021-01-23 09:34:53 +0100

    test-connect bez wycieku pamieci

--- projekt/tests/test-connect.c
+++ projekt/tests/test-connect.c
@@ -10,19 +10,31 @@ int main(int argc, char* argv[])
     char* _explore="http://edi.iem.pw.edu.pl:30000/worlds/api/v1/worlds/explore/qwerty_12";
     char* _rotater="http://edi.iem.pw.edu.pl:30000/worlds/api/v1/worlds/rotate/qwerty_12/right";
     char* _rotatel="http://edi.iem.pw.edu.pl:30000/worlds/api/v1/worlds/rotate/qwerty_12/left";
-    
-    printf("answer for info: \n%s",make_request(_info));
-    printf("answer for reset: \n%s",make_request(_reset));
-    printf("answer for move: \n%s",make_request(_move));
-    printf("answer for explore: \n%s",make_request(_explore));
-    printf("answer for rotate right: \n%s",make_request(_rotater));
-    printf("answer for rotate left: \n%s",make_request(_rotatel));
 
-    if((make_request(_info)!=NULL)&&(make_request(_reset)!=NULL)&&(make_request(_move)!=NULL)&&(make_request(_explore)!=NULL)&&(make_request(_rotater)!=NULL)&&(make_request(_rotatel)!=NULL))
-    printf("Success!\n");
+    char* info=make_request(_info);
+    char*reset=make_request(_reset);
+    char*move = make_request(_move);
+    char*explore = make_request(_explore);
+    char* rotater=make_request(_rotater);
+    char*rotatel=make_request(_rotatel);
+
+    printf("answer for info: \n%s",info);
+    printf("answer for reset: \n%s",reset);
+    printf("answer for move: \n%s",move);
+    printf("answer for explore: \n%s",explore);
+    printf("answer for rotate right: \n%s",rotater);
+    printf("answer for rotate left: \n%s",rotatel);
+
+    if((info!=NULL)&&(reset!=NULL)&&(move!=NULL)&&(explore!=NULL)&&(rotater!=NULL)&&(rotatel!=NULL))
+        printf("Success!\n");
     else
     {
         printf("Failed!\n");
     }
-    
+    free(info);
+    free(reset);
+    free(move);
+    free(explore);
+    free(rotater);
+    free(rotatel);
 }
